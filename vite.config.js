ff7046da95dd7c4cfa35b4d165f66a92004449fe
TUIzFPV <santipap350z@gmail.com>
2026-09-10 07:44:17 +0000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
         background_color: "#050b14",
         display: "standalone",
         start_url: "/",
+        lang: "th",
         icons: [
           { src: "icons/icon-192.png", sizes: "192x192", type: "image/png" },
           { src: "icons/icon-512.png", sizes: "512x512", type: "image/png" },
